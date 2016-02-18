876d7e87190bc183b9a37904e39ad1ad52950dcc
Matthijs Lasure <Matthijs.Lasure@student.uantwerpen.be>
2016-02-18 15:03:01 +0100

    Updated Rotation matrix: het werkt nu (theoretisch :/)
    Compileerd correct op WIN

--- MonteCarlo/lib.f95
+++ MonteCarlo/lib.f95
@@ -12,6 +12,53 @@ module lib
     function RotMatrix(CoM, RelPos, hoek) result(AbsPos)
         TYPE (vector), INTENT(in) :: CoM, hoek
         TYPE (vector), DIMENSION(:), INTENT(in) :: RelPos
-        TYPE (vector) :: AbsPos
+        TYPE (vector), DIMENSION(size(RelPos)) :: AbsPos
+
+        ! Variabelen
+        !===========
+        double precision, dimension(3,3) :: RM ! Rotatiematrix
+        double precision, dimension(3) :: temp ! Tijdelijke array
+        TYPE (vector) :: v ! Tijdelijke vector
+
+        double precision :: cos1, cos2, cos3
+        double precision :: sin1, sin2, sin3
+        integer :: n ! Aantal atomen
+        integer :: i
+
+        n = size(RelPos)
+
+        ! Bereken cos / sin
+        !==================
+        cos1 = cos(hoek%x)
+        cos2 = cos(hoek%y)
+        cos3 = cos(hoek%z)
+        sin1 = sin(hoek%x)
+        sin2 = sin(hoek%y)
+        sin3 = sin(hoek%z)
+
+        ! Opstellen matrix
+        !=================
+        ! Rij 1
+        RM(1,1) = cos1 * cos2 - sin1 * sin2 * cos3
+        RM(1,2) = sin1 * cos2 + cos1 * sin2 * cos3
+        RM(1,3) = sin2 * sin3
+        ! Rij 2
+        RM(2,1) = -1.D0 * cos1 * sin2 - sin1 * cos2 * cos3
+        RM(2,2) = -1.D0 * sin1 * sin2 + cos1 * cos2 * cos3
+        RM(2,3) = cos2 * sin3
+        ! Rij 3
+        RM(3,1) = sin1 * sin3
+        RM(3,2) = -1.D0 * cos1 * sin3
+        RM(3,3) = cos3
+
+        ! Roteren + transleren atomen
+        !============================
+        do i=1,n
+            temp = getArray(RelPos(i))
+            temp = matmul(RM, temp)
+            v = fromArray(temp)
+            AbsPos(i) = CoM + v
+        end do
+
     end function RotMatrix
 end module lib
