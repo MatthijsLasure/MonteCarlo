c996055077d22057229d448f1b700472cb2a899c
Matthijs Lasure <Matthijs.Lasure@student.uantwerpen.be>
2016-07-07 14:15:28 +0200

    Updated tools + added example files

--- Tools/check.sh
+++ Tools/check.sh
@@ -2,7 +2,8 @@
 
 while :
 do
-	line=$(tail -n -1 $1)
-	printf "$line \\r"
+	file=$( grep '^OUT' $1 | tail -1 | tr -s ' ' | cut -d ' ' -f2)
+	line=$(tail -n -1 $file)
+	printf "$file $line \\r"
 	sleep 1
 done
--- Tools/run.sh
+++ Tools/run.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 
-# Check if directory gauss exists, make if not
-if [ ! -d "gauss" ]; then
-	echo "Directory gauss does not exist. Creating..."
-	mkdir gauss
-else
-	echo "Clearing Gauss..."
-	touch gauss/hey
-	rm gauss/*
-fi
+export OMP_NUM_THREADS=8
+export KMP_AFFINITY=scatter
 
-if [ ! -d "IO" ]; then
-	echo "Directory IO does not exist. Creating..."
-	touch IO/hey
-	mkdir IO
-else
-	echo "Clearing IO..."
-	rm IO/*
-fi
+[ ! -d "IO" ] && mkdir IO
+[ ! -d "prep"] && mkdir prep
 
-cp box.txt IO/box.0.in
+# PREP
+cp box.txt prep/box.0.in
+cp solute_mac.txt prep/solute.0.in
+cp solute_mac.txt prep/solute.1.in
+cp solute_mac.txt prep/solute.2.in
+cp solute_mac.txt prep/solute.3.in
+cp solute_mac.txt prep/solute.4.in
 
-./MonteCarloOpenMP config.ini 5000000 0 0
+# r = 20
+./MonteCarloGNU config0.ini 5000000 0 0
 
-cp IO/box.0.out IO/box.1.in
+./BoxScale 0.9 prep/box.0.out prep/box.1.in
 
-./MonteCarloOpenMP config.ini 0 1000 1
+# r = 18
+./MonteCarloGNU config0.ini 5000000 0 1
+
+./BoxScale 0.9 prep/box.1.out prep/box.2.in
+
+# r = 16.2
+./MonteCarloGNU config0.ini 5000000 0 2
+
+./BoxScale 0.960971428 prep/box.2.out prep/box.3.in
+
+# r = 15.57
+./MonteCarloGNU config0.ini 5000000 0 3
+
+# Long Gaussian
+cp prep/box.3.out prep/box.4.in
+./MonteCarloGNU config0.ini 0 25000 4
 
 
+# Prep big
+cp prep/box.4.out IO/box.0.out
+cp solute_mac.txt IO/solute.0.out
+
+# DOIT
+for j in {1..50000};
+do
+	cp IO/box.$(( j - 1 )).out IO/box.$j.in
+	cp IO/solute.$(( j - 1 )).out IO/solute.$j.in
+
+	./MonteCarloGNU config1.ini 0 500 $j
+done
